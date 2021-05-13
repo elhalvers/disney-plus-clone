dc022dca38271d3d9734c6414bc418c7637de130
elhalvers <ericleifhalverson@gmail.com>
2021-05-13 16:45:13 -0700

    Added Viewers component

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -1,7 +1,64 @@
 import React from 'react';
+import styled from 'styled-components';
 
 function Viewers() {
-  return <div>Viewers</div>;
+  return (
+    <Container>
+      <Wrap>
+        <img src='/images/viewers-disney.png' />
+      </Wrap>
+      <Wrap>
+        <img src='/images/viewers-pixar.png' />
+      </Wrap>
+      <Wrap>
+        <img src='/images/viewers-marvel.png' />
+      </Wrap>
+      <Wrap>
+        <img src='/images/viewers-starwars.png' />
+      </Wrap>
+      <Wrap>
+        <img src='/images/viewers-national.png' />
+      </Wrap>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  margin-top: 30px;
+  padding: 30px; 0 26px;
+  display: grid;
+  grid-gap: 25px;
+  grid-template-columns: repeat(5, minmax(0, 1fr));
+`;
+
+const Wrap = styled.div`
+  background-image: linear-gradient(rgb(58, 60, 74), rgb(36, 38, 50));
+  border-radius: 10px;
+  border: 3px solid rgba(249, 249, 249, 0.1);
+  box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+    rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+  cursor: pointer;
+  ${
+    '' /* display: block;
+  overflow: hidden;
+  position: relative;
+  transform: scale(1);
+  width: 100%; */
+  }
+  transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s !important;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  &:hover {
+    transform: scale(1.05);
+    border-color: rgba(249, 249, 249, 0.8);
+    box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px,
+      rgb(0 0 0 / 72%) 0px 30px 22px -10px;
+  }
+`;
 
 export default Viewers;
